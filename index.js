ac6134417470c135c60b2e8f6aa9121593cc7534
Clewerton Roque <clewerton203@gmail.com>
2020-11-21 01:03:37 -0300

    index teste commit

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 
-const currency = require('./src/currency-converter')
 
+
+
+/* Example
+const currency = require('./src/currency-converter')
 async function exemple(){
     var test = await currency.convertCurrencyByCode({value: 4, code: 'USD'},{code: 'BRL'})
     console.log(await test)
 }
 exemple()
+*/
 
 module.exports = require('./src/currency-converter')
